526dad8294756b38fcbc11f185a22c54c97e2f50
zhijieeeeee <tang562250920@gmail.com>
2017-10-18 16:00:41 +0800

    update

--- app/src/main/java/com/don/pedometer/MainActivity.java
+++ app/src/main/java/com/don/pedometer/MainActivity.java
@@ -1,13 +1,26 @@
 package com.don.pedometer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_current)
+    TextView tvCurrent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
